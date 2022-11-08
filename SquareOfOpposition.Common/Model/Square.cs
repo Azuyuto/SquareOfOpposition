0c0d2eb692f0b1f9297118e8f50b140ce3d15450
Mikołaj Białek <mikolaj@freewayit.pl>
2022-11-08 18:02:06 +0100

    Model

---
+++ SquareOfOpposition.Common/Model/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfOpposition.Common.Model
+{
+    public class Square
+    {
+        public string SaP { get; set; } // up-left
+
+        public string SeP { get; set; } // up-right
+
+        public string SiP { get; set; } // down-left
+
+        public string SoP { get; set; } // down-right
+    }
+}
